64278a2e77abc9f8efdee71748a49a5a93a90a97
bishal <bishalkadariya6@gmail.com>
2024-04-15 17:56:41 +1000

    login page

---
+++ component/LoginScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, TextInput, Button } from 'react-native';
+
+const LoginScreen = () => {
+  return (
+    <View>
+      <TextInput placeholder="Username" />
+      <TextInput placeholder="Password" secureTextEntry={true} />
+      <Button title="Login" onPress={() => {/* Handle login */}} />
+    </View>
+  );
+};
+
+export default LoginScreen;
